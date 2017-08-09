bb99370769187aa828e477a922f2e9a75a399a31
maocai <xiamc16@163.com>
2017-08-09 11:07:31 +0800

    Create Contains Duplicate.cpp

---
+++ 2017/August/Array/Contains Duplicate.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************************************
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value 
+appears at least twice in the array, and it should return false if every element is distinct.
+*************************************************************************************************/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> Value;
+        for(int i=0; i<nums.size(); i++){
+            if(++Value[nums[i]] > 1)
+                return true;
+        }
+        return false;
+    }
+};
+
