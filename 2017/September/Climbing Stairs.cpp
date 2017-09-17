2dbe7a8a4443c753ec69e6bd623d7b5efdf519e5
maocai <xiamc16@163.com>
2017-09-17 15:43:42 +0800

    Create Climbing Stairs.cpp

---
+++ 2017/September/Climbing Stairs.cpp
@@ -0,0 +1,28 @@
+/****************************************************
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+***************************************************/
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        
+        int one_step = 2;
+        int two_step = 1;
+        int sum_steps = 0;
+        
+        for(int i=2; i < n; i++){
+            sum_steps = one_step + two_step;
+            two_step = one_step;
+            one_step = sum_steps;
+        }
+        
+        return sum_steps;
+    }
+};
+
+
